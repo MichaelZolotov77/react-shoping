24705bbddfb6e85be17a51670e343791b3afefac
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-08 20:45:42 +0300

    Добавление ссылок

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,18 @@ function Header() {
   return (
     <nav className=" green darken-1">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a
+              href="https://michaelzolotov77.github.io/react-shoping"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
